e1ca84c748a55d7e19ad0c28bf81fd28002bc530
nilrey vm2 <nilrey@vm2.com>
2025-05-16 10:43:47 +0300

    clear routs

--- api/routes/containers.py
+++ api/routes/containers.py
@@ -12,7 +12,7 @@ import psycopg2
 router = APIRouter()
 
 
-@router.get("/docker/images/list")
+@router.get("/images")
 async def list_docker_images():
     try:
         images = docker_service.get_docker_images()
@@ -26,29 +26,29 @@ async def list_docker_images():
             "message": str(e)
         })
 
-@router.get("/docker/images/count")
-async def get_images_count():
-    version = None
-    # try:
-    #     conn = psycopg2.connect(
-    #         host="10.0.0.1",
-    #         database="camino",
-    #         user="postgres",
-    #         password="postgres"
-    #     )
-    #     cur = conn.cursor()
-    #     cur.execute("SELECT version();")
-    #     version = cur.fetchone()
-    #     cur.close()
-    #     conn.close()
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+# @router.get("/docker/images/count")
+# async def get_images_count():
+#     version = None
+#     # try:
+#     #     conn = psycopg2.connect(
+#     #         host="10.0.0.1",
+#     #         database="camino",
+#     #         user="postgres",
+#     #         password="postgres"
+#     #     )
+#     #     cur = conn.cursor()
+#     #     cur.execute("SELECT version();")
+#     #     version = cur.fetchone()
+#     #     cur.close()
+#     #     conn.close()
+#     # except Exception as e:
+#     #     raise HTTPException(status_code=500, detail=str(e))
     
-    images = docker_service.list_images()
-    return {"count": len(images), "db_ver:sion": version}
+#     images = docker_service.list_images()
+#     return {"count": len(images), "db_ver:sion": version}
 
 
-@router.get("/docker/images/{image_id}")
+@router.get("/images/{image_id}")
 async def get_docker_image(image_id: str):
     try:
         image = docker_service.find_image_by_id(image_id)
@@ -72,7 +72,38 @@ async def get_docker_image(image_id: str):
         }
 
 
-@router.get("/docker/containers/list")
+# @router.get("/docker/containers/count")
+# async def get_containers_count():
+#     containers = docker_service.list_containers(all=True)
+#     return {"count": len(containers)}
+
+@router.post("/images/{imageId}/run")
+async def run_container(request: schemas.CreateContainerRequest , imageId: str = Path(...)):
+    try:
+        params = request.model_dump()
+        params["imageId"] = imageId # 0d0eb38589601232c9ad9196d1eaa01db2280d7a2377860ecfe0e93883ef53e3
+        response = docker_service.run_container(params)
+        return {"message": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# @router.post("/docker/container/start")
+# async def start_container(request: schemas.ContainerIdRequest):
+#     try:
+#         result = docker_service.start_container(container_id=request.container_id)
+#         return result
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+# @router.delete("/docker/container/stop")
+# async def stop_container(request: schemas.ContainerIdRequest):
+#     try:
+#         result = docker_service.stop_container(container_id=request.container_id)
+#         return result
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/containers")
 async def get_containers():
     try:
         containers = docker_service.list_running_containers()
@@ -93,39 +124,7 @@ async def get_containers():
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
-
-@router.get("/docker/containers/count")
-async def get_containers_count():
-    containers = docker_service.list_containers(all=True)
-    return {"count": len(containers)}
-
-@router.post("/images/{imageId}/run")
-async def run_container(request: schemas.CreateContainerRequest , imageId: str = Path(...)):
-    try:
-        params = request.model_dump()
-        params["imageId"] = imageId # 0d0eb38589601232c9ad9196d1eaa01db2280d7a2377860ecfe0e93883ef53e3
-        response = docker_service.run_container(params)
-        return {"message": response}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@router.post("/docker/container/start")
-async def start_container(request: schemas.ContainerIdRequest):
-    try:
-        result = docker_service.start_container(container_id=request.container_id)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@router.delete("/docker/container/stop")
-async def stop_container(request: schemas.ContainerIdRequest):
-    try:
-        result = docker_service.stop_container(container_id=request.container_id)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@router.get("/docker/vm/check")
+@router.get("/vm/check")
 async def get_vm_without_ann():
     docker_service.logging.info("Начало обработки")
     result, is_error = docker_service.find_vm_without_ann_images() 
